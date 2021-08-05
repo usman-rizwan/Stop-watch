c4b541c4edf2cd129669947e14cb284f642fc12a
usman_rizwan <81845304+usman-rizwan@users.noreply.github.com>
2021-08-05 16:44:45 +0500

    Add files via upload

---
+++ stopWatch/app.js
@@ -0,0 +1,54 @@
+var hour=0;
+var min=0;
+var sec=0;
+var msec=0;
+var hourHeading = document.getElementById("hour");
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+
+    var interval ;
+function timer (){
+    msec++;
+    msecHeading.innerHTML = msec;
+        if (msec >=100 ){
+            sec++;
+            secHeading.innerHTML = sec;
+            msec=0;
+    }else if (sec >= 60){
+        min++;
+        sec=0;
+        minHeading.innerHTML = min;
+    }
+    else if (min >= 60){
+        min=0;
+        hour++;
+        hourHeading.innerHTML = hour;
+    }
+}
+
+
+function start(){
+    interval = setInterval(timer,10);
+    document.getElementById("start").disabled=false;
+    document.getElementById("start").disabled=true;
+   
+
+}
+function stop(){
+    clearInterval(interval);
+    document.getElementById("start").disabled=false;
+}
+function reset(){
+    min = 0;
+    sec = 0;
+    msec = 0;
+    minHeading.innerHTML = min;
+    secHeading.innerHTML = sec;
+    msecHeading.innerHTML = msec;
+    stop();
+    document.getElementById("start").disabled=false;
+}
+
+
+
